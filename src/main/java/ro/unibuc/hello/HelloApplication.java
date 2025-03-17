bb0c0d86ef93f7e80793395f64db0d2c6c7371fa
Serban096 <128298814+Serban096@users.noreply.github.com>
2025-03-17 19:56:33 +0000

    small fix

--- src/main/java/ro/unibuc/hello/HelloApplication.java
+++ src/main/java/ro/unibuc/hello/HelloApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import ro.unibuc.hello.data.InformationEntity;
 import ro.unibuc.hello.data.InformationRepository;
+import ro.unibuc.hello.data.WeatherDataRepository;
+import ro.unibuc.hello.data.WeatherDataEntity;
 
 import jakarta.annotation.PostConstruct;
 
@@ -16,6 +18,9 @@ public class HelloApplication {
 	@Autowired
 	private InformationRepository informationRepository;
 
+	@Autowired
+	private WeatherDataRepository weatherDataRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
@@ -25,6 +30,8 @@ public class HelloApplication {
 		informationRepository.deleteAll();
 		informationRepository.save(new InformationEntity("Overview",
 				"This is an example of using a data storage engine running separately from our applications server"));
-	}
+		weatherDataRepository.deleteAll();
+		weatherDataRepository.save(new WeatherDataEntity());
+		}
 
 }
